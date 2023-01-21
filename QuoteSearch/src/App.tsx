fde390c882cc3ca95aea5cccc95372f44d65f5da
Carson Wallace <a02256811@aggies.usu.edu>
2023-01-21 10:21:14 -0700

    added random quote

--- QuoteSearch/src/App.tsx
+++ QuoteSearch/src/App.tsx
@@ -1,19 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function App() {
   const [inputQuote, setInputQuote] = useState("");
+  const [quotes, setQuotes] = useState("");
+  const [randQuote, setRandQuote] = useState("");
+
+  useEffect(() => {
+    getRandom();
+  }, []);
 
   function submit() {
     console.log("submitted");
-    //TODO: add API request
+    getRandom();
+  }
+
+  function getRandom() {
+    fetch("https://usu-quotes-mimic.vercel.app/api/random")
+    .then(r => r.json())
+    .then(quote => setRandQuote(quote));
   }
 
   return (
     <div className="App">
-      <div>
+      <div className="Submit">
+        <header>Quote Search</header>
         <form onSubmit={submit}>
           <input value={inputQuote} onChange={e => setInputQuote(e.target.value)} placeholder='Albert Einstein' type="text"/>
         </form>
+      </div>
+
+      <div className='Random'>
+        <p className='quote'>{randQuote.content}</p>
+        <p>- {randQuote.author}</p>
+      </div>
+
+      <div className="Results">
       </div>
     </div>
   )
